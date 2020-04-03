85095dca5a5c33a16050e52f716905f1bd3efbb4
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2020-04-03 19:33:44 -0300

    Novos botões e listeners para teclas

--- src/js/index.js
+++ src/js/index.js
@@ -14,18 +14,64 @@ const desenharQuadrado = () => {
 
   ctx.fillStyle = '#333'
   ctx.fillRect(posicao.x, posicao.y, 50, 50)
+
+  window.requestAnimationFrame(desenharQuadrado)
+}
+
+const aumentarEixoY = () => {
+  posicao.y = posicao.y + 10
+}
+
+const aumentarEixoX = () => {
+  posicao.x = posicao.x + 10
+}
+
+const diminuirEixoY = () => {
+  posicao.y = posicao.y - 10
+}
+
+const diminuirEixoX = () => {
+  posicao.x = posicao.x - 10
 }
 
 window.onload = () => {
-  document.querySelector('#bte').addEventListener('click', () => {
-    posicao.x = posicao.x - 10
-    desenharQuadrado()
+  window.requestAnimationFrame(desenharQuadrado)
+
+  window.addEventListener('click', evt => {
+    if (evt.target.id === 'btup') {
+      diminuirEixoY()
+    }
+
+    if (evt.target.id === 'btdown') {
+      aumentarEixoY()
+    }
+
+    if (evt.target.id === 'btleft') {
+      diminuirEixoX()
+    }
+
+    if (evt.target.id === 'btright') {
+      aumentarEixoX()
+    }
   })
 
-  document.querySelector('#btd').addEventListener('click', () => {
-    posicao.x = posicao.x + 10
-    desenharQuadrado()
-  })
+  window.addEventListener('keydown', evt => {
+    const lower = key => key.toLowerCase()
 
-  desenharQuadrado()
+    if (lower(evt.key) === 'arrowup' || lower(evt.key) === 'w') {
+      diminuirEixoY()
+    }
+
+    if (lower(evt.key) === 'arrowdown' || lower(evt.key) === 's') {
+      aumentarEixoY()
+    }
+
+    if (lower(evt.key) === 'arrowleft' || lower(evt.key) === 'a') {
+      diminuirEixoX()
+    }
+
+    if (lower(evt.key) === 'arrowright' || lower(evt.key) === 'd') {
+      aumentarEixoX()
+    }
+  })
 }
